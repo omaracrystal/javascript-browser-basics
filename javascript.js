c0de9e567a38ba850f6e2803c51ef3d5930429bd
omaracrystal <omaracrystal@gmail.com>
2015-07-09 15:56:06 -0600

    added js and added js script to html

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var userInput = prompt("What is your name?")
+
+alert("hello " + userInput)
+// function test()
+// {
+//   var userInput - document.getElementById("userInput").value;
+//   document.write(userInput);
+// }
